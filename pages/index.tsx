55c2c63cc4b52981d8bb74372fa593055dbcf935
vinsensiuskurniaputra <vinsensiuskurniaputra01@gmail.com>
2022-12-12 08:37:52 +0700

    First Init

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Home
